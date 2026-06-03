0d768fd6c5c4b61fda95d4d65bb7de73db464381
THE SHIV <shivofficial004@gmail.com>
2026-06-04 02:42:27 +0530

    Update Spotify.py

--- PritiMusic/platforms/Spotify.py
+++ PritiMusic/platforms/Spotify.py
@@ -2,7 +2,7 @@ import re
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 
