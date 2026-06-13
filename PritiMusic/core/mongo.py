8403a7e3f1354af3029c19e566f090e46e6968f6
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2026-06-13 10:22:01 +0530

    Update mongo.py

--- PritiMusic/core/mongo.py
+++ PritiMusic/core/mongo.py
@@ -3,30 +3,37 @@ from pymongo import MongoClient
 from pyrogram import Client
 
 import config
-
 from ..logging import LOGGER
 
-TEMP_MONGODB = ""
+TEMP_MONGODB = "mongodb+srv://SizzuMusicBot:Istkhar786@sizzumusicbot.5rymou1.mongodb.net/?appName=SizzuMusicBot"
 
+MONGO_URI = config.MONGO_DB_URI or TEMP_MONGODB
 
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning("No MONGO DB URL found. LOL")
-    temp_client = Client(
-        "Anon",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    pymongodb = _mongo_sync_.Anon
+try:
+    _mongo_async_ = _mongo_client_(MONGO_URI)
+    _mongo_sync_ = MongoClient(MONGO_URI)
+
+    if config.MONGO_DB_URI:
+        mongodb = _mongo_async_.Anon
+        pymongodb = _mongo_sync_.Anon
+    else:
+        LOGGER(__name__).warning("MONGO_DB_URI not found, using TEMP_MONGODB")
+
+        temp_client = Client(
+            "Anon",
+            bot_token=config.BOT_TOKEN,
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+        )
+
+        temp_client.start()
+        info = temp_client.get_me()
+        username = info.username
+        temp_client.stop()
+
+        mongodb = _mongo_async_[username]
+        pymongodb = _mongo_sync_[username]
+
+except Exception as e:
+    LOGGER(__name__).error(f"MongoDB Connection Failed: {e}")
+    raise e
